0ca67d6a3b940311b8017275666e1865e8c2bb8e
Ze Zhou <zezhou96@gmail.com>
2022-03-01 09:44:54 -0700

    Created batch demo

---
+++ dmit2015-1212-batch-demo/src/main/java/common/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package common.config;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.annotation.sql.DataSourceDefinitions;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@DataSourceDefinitions({
+
+	@DataSourceDefinition(
+		name="java:app/datasources/mssqlDS",
+		className="com.microsoft.sqlserver.jdbc.SQLServerDataSource",
+		url="jdbc:sqlserver://DMIT-Capstone1.ad.sast.ca;databaseName=DMIT2015_1212_A01_zzhou18;TrustServerCertificate=true",
+		user="zzhou18",
+		password="RemotePassword200487125"),
+
+})
+
+@ApplicationScoped
+public class ApplicationConfig {
+
+}
